4fbc35bd609a4f7dcddedc5903eaa45fb4575bdb
Pongsaphak <39190909+pongsaphakmw@users.noreply.github.com>
2022-03-25 18:32:16 +0700

    Add files via upload
    Twan part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import login
+from menu import main_menu
 
 if __name__ == '__main__':
     print('Welcome to our game')
-    login.Register()
-    login.Login()
+    main_menu.login_menu()
--- menu.py
+++ menu.py
@@ -1,6 +1,23 @@
-# import login
+import login
 
-def main_menu():
-    print('------------------------')
+class main_menu:
+    def login_menu():
+        print('\t\t\tWelcome to Nongbate in wonderland')
+        print('\t\tRegister type : 1')
+        print('\t\tLogin type : 2')
+        while 1:
+            choose = int(input('\t\tEnter your choose here : '))
+            print('\t----------------------------------------------')
+            if choose == 1:
+                login.Register()
+                break
+            elif choose == 2 :
+                login.Login()
+                break
+            else:
+                print('Wrong input!')
+                continue
+    
+    def game_menu():
+        print('\t\t\tChoose mini games to play')
 
-main_menu()
